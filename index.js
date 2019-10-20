37b7488bf1464a4c0055774221edb5147ae05d08
jeremymgibbons <jeremymgibbons@gmail.com>
2019-10-19 21:35:00 -0700

    added test files, eslint file, and beautified code,  minor fixes

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ var mime = require('mime');
 var wss = require('./websockets-server');
 
 var handleError = function(err, res) {
-    res.writeHead(404, {
-        'Content-Type': 'text/html'
-      });
-    //bronse challenge send error to error.html
-    res.end('<meta http-equiv="Refresh" content="0; url=/error.html" />');
-  };
+  res.writeHead(404, {
+    'Content-Type': 'text/html'
+  });
+  //bronse challenge send error to error.html
+  res.end('<meta http-equiv="Refresh" content="0; url=/error.html" />');
+};
 
-var server = http.createServer(function (req, res) {
-    console.log('Responding to a request.');
-    var filePath = extract(req.url);
-    fs.readFile(filePath, function (err, data) {
-        if (err) {
-            handleError(err, res);
-            return;
-        } else {
-            //silver challenge set Content-Type dynamically
-            var contentType = mime.getType(filePath)
-            res.setHeader('Content-Type', contentType);
-            res.end(data);
-        }
-    });
+var server = http.createServer(function(req, res) {
+  console.log('Responding to a request.');
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      //silver challenge set Content-Type dynamically
+      var contentType = mime.getType(filePath)
+      res.setHeader('Content-Type', contentType);
+      res.end(data);
+    }
+  });
 });
 server.listen(3000);
--- websockets-server.js
+++ websockets-server.js
@@ -3,7 +3,7 @@ var WebSocket = require('ws');
 var WebSocketServer = WebSocket.Server;
 var port = 3001;
 var ws = new WebSocketServer({
-    port: port
+  port: port
 });
 
 var messages = [];
@@ -11,33 +11,33 @@ var topicFlag = false;
 
 console.log('websockets server started');
 
-ws.on('connection', function (socket) {
-    console.log('client connection established');
+ws.on('connection', function(socket) {
+  console.log('client connection established');
 
-    messages.forEach(function (msg) {
-        socket.send(msg);
-    });
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  });
 
-    socket.on('message', function (data) {
-        console.log(`message received: ${data}`);
-        if (data.substring(0, 6) === '/topic') {
-            topic = data.substring(7);
-            messages.push(data);
-            ws.clients.forEach(function (clientSocket) {
-                clientSocket.send(`*** Topic has changed to '${data.substring(7)}'`);
-            });
-            if (topicFlag) {
-                messages[0] = `*** Topic is '${data.substring(7)}'`;
-            } else {
-                messages.unshift(`*** Topic is '${data.substring(7)}'`);
-            }
-            topicFlag = true;
-            
-        } else {
-            messages.push(data);
-            ws.clients.forEach(function (clientSocket) {
-                clientSocket.send(data);
-            });
-        }
-    });
+  socket.on('message', function(data) {
+    console.log(`message received: ${data}`);
+    if (data.substring(0, 6) === '/topic') {
+      topic = data.substring(7);
+      messages.push(data);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(`*** Topic has changed to '${data.substring(7)}'`);
+      });
+      if (topicFlag) {
+        messages[0] = `*** Topic is '${data.substring(7)}'`;
+      } else {
+        messages.unshift(`*** Topic is '${data.substring(7)}'`);
+      }
+      topicFlag = true;
+
+    } else {
+      messages.push(data);
+      ws.clients.forEach(function(clientSocket) {
+        clientSocket.send(data);
+      });
+    }
+  });
 });
